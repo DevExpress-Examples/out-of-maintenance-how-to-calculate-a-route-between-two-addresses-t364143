faa2cf77a5866b31c4f47df625fe0890fba8f63d
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RouteByAddresses/Form1.cs
@@ -0,0 +1,43 @@
+﻿using DevExpress.XtraMap;
+using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ZoomToFitOnRouteCalculated {
+    public partial class Form1 : Form {
+        const String yourBingKey = "Insert Your Key Here";
+
+        public Form1() {
+            InitializeComponent();
+            imageProvider.BingKey = yourBingKey;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            // Create a new route provider.
+            BingRouteDataProvider provider = new BingRouteDataProvider { BingKey = yourBingKey };
+            informationLayer.DataProvider = provider;
+
+            // Send a request and handle request result.
+            provider.RouteCalculated += OnRouteCalculated;
+            // Note that waypoints are created using address instead of the location.
+            provider.CalculateRoute(new List<RouteWaypoint>() {
+                new RouteWaypoint("New York", "Belmont Park, New York, USA"),
+                new RouteWaypoint("Las Vegas", "Lorenzi Park, Las Vegas, USA")});
+            splashScreenManager1.ShowWaitForm();
+        }
+
+        void OnRouteCalculated(object sender, BingRouteCalculatedEventArgs e) {
+            SearchBoundingBox box = e.CalculationResult.RouteResults[0].BoundingBox;
+            GeoPoint topLeft = new GeoPoint {
+                Latitude = box.NorthLatitude,
+                Longitude = box.WestLongitude
+            };
+            GeoPoint bottomRight = new GeoPoint {
+                Latitude = box.SouthLatitude,
+                Longitude = box.EastLongitude
+            };
+            mapControl.ZoomToRegion(topLeft, bottomRight, 0.4);
+            splashScreenManager1.CloseWaitForm();
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,13 @@
+# How to calculate a route between two addresses
+
+
+This example demonstrates how to calculate a route between two addresses.
+
+
+<h3>Description</h3>
+
+<p>To do this, use the&nbsp;<a href="https://documentation.devexpress.com/#WindowsForms/DevExpressXtraMapBingRouteDataProvider_CalculateRoutetopic">BingRouteDataProvider.CalculateRoute</a>&nbsp;method and specify a&nbsp;request containing the required addresses.</p>
+
+<br/>
+
+
